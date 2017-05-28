f5c4d0bfa61ddacd8398c164123035f5b23425d6
Mark <markd@marks-macbook-pro.local>
2017-05-28 16:00:39 -0600

    Set view controllers to implement UITextField delegate methods. Added CCSMStorage object and began calling it.

--- Relay/src/view controllers/LoginValidationViewController.h
+++ Relay/src/view controllers/LoginValidationViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LoginValidationViewController : UIViewController
+@interface LoginValidationViewController : UIViewController <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *validationLabel;
 @property (weak, nonatomic) IBOutlet UITextField *validationCodeTextField;
--- Relay/src/view controllers/LoginViewController.m
+++ Relay/src/view controllers/LoginViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "LoginViewController.h"
+#import "CCSMStorage.h"
 
 @interface LoginViewController ()
+
+@property (strong) CCSMStorage *ccsmStorage;
 
 @end
 
@@ -18,12 +21,22 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    
+    
+    
+    self.ccsmStorage = [[CCSMStorage alloc] init];
+    
     // Allow for localized strings on controls
-//    self.organizationLabel.text = NSLocalizedString(@"Organization", @"");
-//    self.usernameLabel.text = NSLocalizedString(@"Username", @"");
     self.organizationTextField.placeholder = NSLocalizedString(@"Enter Organization", @"");
     self.usernameTextField.placeholder = NSLocalizedString(@"Enter Username", @"");
     self.loginButton.titleLabel.text = NSLocalizedString(@"Login", @"");
+}
+
+-(void)viewDidDisappear:(BOOL)animated
+{
+    [self.spinner stopAnimating];
+    
+    [super viewDidDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,8 +56,42 @@
 
 -(IBAction)OnLoginButtonTap:(id)sender
 {
-    // Do stuff on Login button tap    
+    // Do stuff on Login button tap
+    [self.spinner startAnimating];
+
+    [self.ccsmStorage setOrgName:self.organizationTextField.text];
+    [self.ccsmStorage setUserName:self.usernameTextField.text];
     [self performSegueWithIdentifier:@"validationViewSegue" sender:nil];
+    [self.spinner stopAnimating];
 }
+
+-(BOOL)isValidOrganization:(NSString *)organization
+{
+    return YES;
+}
+
+-(BOOL)isValidUsername:(NSString *)username
+{
+    return YES;
+}
+
+#pragma mark - UITextField delegate methods
+-(BOOL)textFieldShouldEndEditing:(UITextField *)textField
+{
+    if (textField == self.organizationTextField)
+    {
+        NSLog(@"OrgFieldEnding...");
+        return [self isValidOrganization:textField.text];
+    }
+    else if (textField == self.usernameTextField)
+    {
+        NSLog(@"UserFiendEnding...");
+        return [self isValidUsername:textField.text];
+    }
+    else
+        return YES;
+}
+
+#pragma mark - Lazy instatiation
 
 @end
