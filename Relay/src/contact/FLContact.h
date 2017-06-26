e36f7dc72ea61f03efbc5821bea9469464127e34
Mark Descalzo <mdescalzo@mac.com>
2017-06-26 15:18:19 -0600

    New Contact object to handle CCSM contacts.

---
+++ Relay/src/contact/FLContact.h
@@ -0,0 +1,15 @@
+//
+//  FLContact.h
+//  Forsta
+//
+//  Created by Mark on 6/26/17.
+//  Copyright © 2017 Forsta. All rights reserved.
+//
+
+#import "Contact.h"
+
+@interface FLContact : Contact
+
+@property (nonatomic, strong) NSString *tag;
+
+@end
---
+++ Relay/src/contact/FLContact.m
@@ -0,0 +1,13 @@
+//
+//  FLContact.m
+//  Forsta
+//
+//  Created by Mark on 6/26/17.
+//  Copyright © 2017 Forsta. All rights reserved.
+//
+
+#import "FLContact.h"
+
+@implementation FLContact
+
+@end
