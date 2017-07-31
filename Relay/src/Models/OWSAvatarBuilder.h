005d30d6fbc38cdf33318c0b1a002e0ac37d3c01
Mark Descalzo <mdescalzo@mac.com>
2017-07-31 14:39:50 -0600

    New and improved avatar builder from Signals.

--- Relay/src/Models/OWSAvatarBuilder.h
+++ Relay/src/Models/OWSAvatarBuilder.h
@@ -1,14 +1,18 @@
-//  Created by Michael Kirk on 9/26/16.
-//  Copyright © 2016 Open Whisper Systems. All rights reserved.
+//
+//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//
 
 NS_ASSUME_NONNULL_BEGIN
 
 @class TSThread;
 @class OWSContactsManager;
+@class UIImage;
 
 @interface OWSAvatarBuilder : NSObject
 
-+ (UIImage *)buildImageForThread:(TSThread *)thread contactsManager:(OWSContactsManager *)contactsManager;
++ (UIImage *)buildImageForThread:(TSThread *)thread
+                 contactsManager:(OWSContactsManager *)contactsManager
+                        diameter:(CGFloat)diameter;
 
 - (nullable UIImage *)buildSavedImage;
 - (UIImage *)buildDefaultImage;
