c6b9ceb96168beafb17e002919de5a6c596ba923
Mark Descalzo <mdescalzo@mac.com>
2018-07-11 12:39:48 -0600

    MainAppContext without errors.

---
+++ Relay/src/util/MainAppContext.h
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//
+
+#import "AppContext.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainAppContext : NSObject <AppContext>
+
+@end
+
+NS_ASSUME_NONNULL_END
